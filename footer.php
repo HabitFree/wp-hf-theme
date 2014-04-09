1c2e7d5f6b187b39333c60d260fc18918b80291d
Nathan <narthur.a@gmail.com>
2014-04-09 17:29:56 -0500

    added unit test results to footer

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
 <div class="clear"></div>
 </div>
 <footer id="footer" role="contentinfo">
+<?php
+	echo do_shortcode( '[simpletest name="SimpleTest Unit Tests" path="/hf-accountability/php/tests.php"]' );
+?>
 <div id="copyright">
 <?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'blankslate' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?>
 </div>
